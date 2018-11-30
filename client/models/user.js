4c129e802f83923c12ca050e24d628139a201223
Matt Gasper <mrgasper27@gmail.com>
2018-11-30 11:59:01 -0600

    Added models

---
+++ client/models/user.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var user = new Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        required: "Username is required"
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is required"
+    },
+
+    pictureUrl: String,
+
+    timeStamp: {
+        type: Date,
+        default: Date.now
+    },
+
+    bioText: {
+        type: String,
+        trim: true,
+    }
+});
+
+var user = mongoose.model("User", user);
+
+module.exports = user;
