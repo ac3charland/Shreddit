f09e22fd117279fb04426922fb2f8eb03c50278d
MNelson <melissa.nelson13@yahoo.com>
2018-11-30 14:06:37 -0600

    added placehoder shreds to profile page (still needs styling)

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./Profile.css"
 import UserBio from "../../components/UserBio";
+import Shred from "../../components/Shred";
 
 class Profile extends Component {
 
     state = {
         name: "Melissa",
         joinDate: "11/30/18",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum sed nulla et rutrum. Phasellus interdum ex et lacus pellentesque sodales. Fusce sollicitudin suscipit ligula sit amet dictum. Donec sagittis ligula ut sapien rhoncus interdum. In id purus vel tellus molestie consequat. Aenean commodo ante ac tincidunt tincidunt. Quisque diam odio, elementum sed placerat in, rhoncus sit amet odio. Sed et turpis vel augue fermentum bibendum. Nam congue tortor vel enim molestie vestibulum."
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum sed nulla et rutrum. Phasellus interdum ex et lacus pellentesque sodales. Fusce sollicitudin suscipit ligula sit amet dictum. Donec sagittis ligula ut sapien rhoncus interdum. In id purus vel tellus molestie consequat. Aenean commodo ante ac tincidunt tincidunt. Quisque diam odio, elementum sed placerat in, rhoncus sit amet odio. Sed et turpis vel augue fermentum bibendum. Nam congue tortor vel enim molestie vestibulum.",
+        shreds: [{shred: "Shred1"}, {shred: "Shred2"}, {shred:"Shred3"}]
 
     }
 
@@ -23,9 +25,18 @@ class Profile extends Component {
                     <div className="row">
 
                         <div className="col s8">
+                        {this.state.shreds.length ? (
+                            this.state.shreds.map(shred => (
+                                <Shred
+                                    shred={shred.shred}
+                                />
+                            ))
+                        ) : (
+                            <h3>You do not have any Shreds yet!</h3>
+                        )}
                         
                         </div>
-
+ 
                         <UserBio
                         name={this.state.name}
                         joinDate={this.state.joinDate}
