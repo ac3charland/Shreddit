48616f570ed01d2de4d1bb343074cee3f8d3df8b
MNelson <melissa.nelson13@yahoo.com>
2018-12-03 12:23:58 -0600

    added to main page and profile page

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
-import "./Main.css"
+import Banner from "../../components/Banner";
+import Shred from "../../components/Shred";
+//import ShredPlayer from "../../components/ShredPlayer";
+import API from "../../utils/API"
+import "./Main.css";
 
 class Main extends Component {
 
     state = {
+        shreds: [{id: 1, shred: "Shred1", votes: 120}, {id: 2, shred: "Shred2", votes: 30}, {id: 3, shred:"Shred3", votes: 80}],
+    }
+
+    upvote = (id, votes) => {
+        let newVotes = votes + 1;
+        API.vote(newVotes, id)
+            .then(console.log("upvoted #" + id))
+            .catch(err => console.log(err));
+    }
+
+    downvote = (id, votes) => {
+        let newVotes = votes - 1;
+        API.vote(newVotes, id)
+        .then(console.log("upvoted #" + id))
+        .catch(err => console.log(err));
 
     }
 
     render(){
         return (
-            
             <div>
-                <h1>Main Page</h1>
+
+                <div>
+                    <Banner/>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col s12">
+                        {this.state.shreds.length ? (
+                            this.state.shreds.map(shred => (
+                                <Shred
+                                    key={shred.id}
+                                    id={shred.id}
+                                    shred={shred.shred}
+                                    votes={shred.votes}
+                                    upvote={this.upvote}
+                                    downvote={this.downvote}
+                                />
+                            ))
+                        ) : (
+                            <h3>No Shreds Posted Yet!</h3>
+                        )}
+                        
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
