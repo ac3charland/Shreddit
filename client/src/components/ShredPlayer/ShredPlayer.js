0c52117482fb92f5281be3d61db9f39cece685ef
Alex Charland <ac3charland@gmail.com>
2018-12-03 13:03:51 -0600

    Finished up working ShredPlayer

--- client/src/components/ShredPlayer/ShredPlayer.js
+++ client/src/components/ShredPlayer/ShredPlayer.js
@@ -7,34 +7,113 @@ class ShredPlayer extends React.Component {
     
     state = {
         isPlaying: false,
-        matrix: []
+        icon: "play_arrow",
+        matrix: [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
     }
 
-    hello() {
-        console.log("Hello!")
-    }
+    // The notes of our sequencer, from lowest to highest
+    notes = ['C4', 'D4', 'E4', 'G4', 'A4', 'C5'];
+
+    sequencer; 
+
+    synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
     
     componentDidMount() {
-        var sequencer = new Nexus.Sequencer('#target', {
-            'size': [400, 200],
+        this.sequencer = new Nexus.Sequencer('#target', {
+            'size': [600, 200],
             'mode': 'toggle',
             'rows': 6,
             'columns': 16
         });
+        
         // This is how we'll load melodies from Mongo
-        sequencer.matrix.set.all(this.state.matrix);
+        this.sequencer.matrix.set.all(this.state.matrix);
 
-        var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
+        this.sequencer.colorize("accent", "#4dd0e1");
 
-        synth.triggerAttackRelease("C4", '8n');
+        // Plays all the notes at a step in the sequencer
+        const stepToSound = (stepArray) => {
+            var notesToPlay = [];
+            for (var i = 0; i < stepArray.length; i++) {
+                var note = this.notes[i];
+                if (stepArray[i] === 1) {
+                    notesToPlay.push(note);
+                }
+            }
+            this.synth.triggerAttackRelease(notesToPlay, '8n')
+        }
+
+        // Triggers a note when a user activates a cell
+        const changeToSound = (cell) => {
+            // If the cell has been turned on:
+            if (cell.state && !this.state.isPlaying) {
+                var row = cell.row;
+                var notesIndex = this.notes.length - row - 1
+                var note = this.notes[notesIndex];
+                this.synth.triggerAttackRelease([note], '8n');
+            }
+        }
+
+        const exportMatrix = (matrix) => {
+            let exportArray = [];
+            for (let i = 0; i < matrix.length; i++) {
+                let row = [];
+                for (let j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j]) {
+                        row.push(1);
+                    } else {
+                        row.push(0);
+                    }
+                }
+                exportArray.push(row);
+            }
+            return exportArray;
+        }
+
+
+        this.sequencer.on('change', function (cell) {
+            changeToSound(cell);
+        });
+
+        this.sequencer.on('step', function (stepArray) {
+            console.log(stepArray);
+            stepToSound(stepArray);
+        });
+
+        this.synth.triggerAttackRelease("C4", '8n');
     }
+
+    
+
+    playButtonClicked = () => {
+        if (this.state.isPlaying === false) {
+            this.sequencer.start();
+            this.setState({
+                'isPlaying': true,
+                'icon': 'pause'
+            })
+        } else {
+            this.sequencer.stop();
+            this.setState({
+                'isPlaying': false,
+                'icon': 'play_arrow'
+            })
+        }
+    }
+
     render() {
         return (
             <div className="player">
                 <div id="target"></div>
 
-                <div id="play" className="button btn-floating btn-large" onClick={this.hello}><i class="material-icons">play_arrow</i></div>
-                <div id="pause" className="button btn-floating btn-large" onClick={this.hello}><i class="material-icons">pause</i></div>
+                <div id="play" className="button btn-floating btn-large cyan lighten-2" onClick={this.playButtonClicked}><i className="material-icons">{this.state.icon}</i></div>
 
             </div>
         )
