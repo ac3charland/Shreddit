916645c5f634d9014505f0c9521cf8211ed88fa5
MNelson <melissa.nelson13@yahoo.com>
2018-12-03 14:12:09 -0600

    changed layout of profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Profile.css"
-import UserBio from "../../components/UserBio";
+//import UserBio from "../../components/UserBio";
 import Shred from "../../components/Shred";
 import Banner from "../../components/Banner";
 
@@ -10,7 +10,7 @@ class Profile extends Component {
         name: "Melissa",
         joinDate: "11/30/18",
         bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum sed nulla et rutrum. Phasellus interdum ex et lacus pellentesque sodales. Fusce sollicitudin suscipit ligula sit amet dictum. Donec sagittis ligula ut sapien rhoncus interdum. In id purus vel tellus molestie consequat. Aenean commodo ante ac tincidunt tincidunt. Quisque diam odio, elementum sed placerat in, rhoncus sit amet odio. Sed et turpis vel augue fermentum bibendum. Nam congue tortor vel enim molestie vestibulum.",
-        shreds: [{shred: "Shred1", votes: 120}, {shred: "Shred2", votes: 30}, {shred:"Shred3", votes: 80}],
+        shreds: [{id: 1, shred: "Shred1", votes: 120}, {id: 2, shred: "Shred2", votes: 30}, {id: 3, shred:"Shred3", votes: 80}],
 
     }
 
@@ -23,14 +23,18 @@ class Profile extends Component {
                 </div>
 
                 <div className="container">
+                    <h2>{this.state.name}'s Shreds</h2>
                     <div className="row">
-
-                        <div className="col s8">
+                        <div className="col s12">
                         {this.state.shreds.length ? (
                             this.state.shreds.map(shred => (
                                 <Shred
-                                    shred={shred.shred}
-                                    votes={shred.votes}
+                                key={shred.id}
+                                id={shred.id}
+                                shred={shred.shred}
+                                votes={shred.votes}
+                                upvote={this.upvote}
+                                downvote={this.downvote}
                                 />
                             ))
                         ) : (
@@ -39,11 +43,11 @@ class Profile extends Component {
                         
                         </div>
  
-                        <UserBio
+                        {/* <UserBio
                         name={this.state.name}
                         joinDate={this.state.joinDate}
                         bio={this.state.bio}
-                        />
+                        /> */}
 
                     </div>
                 </div>
