bd7bf99b93a801f81d634fb8cd6b2fad394bcf47
Alex Charland <ac3charland@gmail.com>
2018-12-06 14:57:08 -0600

    Fixed bug hopefully.

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.use(bodyParser.json());
 app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 } else {
   app.use(express.static(path.join(__dirname, 'public')));
 }
