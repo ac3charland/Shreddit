4f8212053e3436d695c27dd231f9cac92d445eff
Mary Carr <m.kat44@gmail.com>
2018-12-06 00:28:03 -0600

    Added binary math! Some of it's hard-coded but it looks cool and it works!

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -15,6 +15,7 @@ class Main extends Component {
 
     componentDidMount(){
         this.getAllShreds();
+        this.decoding();
     }
 
     getAllShreds = () => {
@@ -49,6 +50,30 @@ class Main extends Component {
 
     walkieTalkie = matrix => {
         this.setState({matrix: matrix});
+    }
+
+    //Binary Math: decoding strings
+    decoding = () => {
+        //dummy shortened matrix
+        var fakeSong = [0, 512, 2146, 0, 8, 32];
+        var decodedMatrix = [];
+
+        for (let i = 0; i < fakeSong.length; i++) {
+            var matrixRow = fakeSong[i].toString(2).split('').map(function(x) {
+                return parseInt(x);
+            })
+
+            while (matrixRow.length < 16) {
+                matrixRow.unshift(0);
+            }
+
+            while (matrixRow.length > 16) {
+                matrixRow.shift();
+            }
+
+            decodedMatrix.push(matrixRow);
+            console.log(decodedMatrix);
+        }
     }
 
     render(){
