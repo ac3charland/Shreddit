ceb97c0a37651407c458a66ab3dc24d38a34eb5d
Max Lettenberger <maxlettenberger@gmail.com>
2018-12-06 12:51:33 -0600

    Cleaning up code for setting token

--- client/src/components/Modals/Register.js
+++ client/src/components/Modals/Register.js
@@ -23,21 +23,16 @@ class Register extends React.Component {
     saveUser = (event) => {
         event.preventDefault();
 
-        // Get current component here to access in tricky scope area
-        let currentComp = this;
+        // Get current component to use in deeper scope area
+        const currentComp = this;
 
         let user = {
             username: this.state.username,
             password: this.state.password
         }
 
-        // Encoding password for saving in localStorage
-        let passwordLS = "ac" + user.password + "x0!"
-
-        // Saving un and pw in localStorage
+        // Saving un in localStorage
         localStorage.setItem("username", user.username);
-        localStorage.setItem("password", passwordLS);
-
 
         // Function to make api call to save new user
         // Req includes user obj with un and pw
@@ -45,22 +40,16 @@ class Register extends React.Component {
         API.saveUser({ user: user })
         .then(function(res){
             if (res.data.user) {
-
                 // Save token in localStorage
                 localStorage.setItem("token", res.data.user.token);
-
+                // Set state.registered to true
                 currentComp.setState({registered: true})
-
-                // Setting state to trigger redirect
-
+                // Set state.readyToRedirect to true, to trigger redirect
                 currentComp.setState({readyToRedirect: true})
-
+                // Reload window to update navbar
                 window.location.reload();
             }
         })
-
-       
-
     }
 
     render(){
